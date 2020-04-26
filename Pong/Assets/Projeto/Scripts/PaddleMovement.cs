ec06d44002d3e3ec15b6f4566bcf2c22588ebe00
Diogo Rosa Carvalho <diodude.lol@gmail.com>
2020-04-26 18:46:21 +0100

    Assets and Paddle Movement
    Import assets to be used (course assets)
    Set up backgroup;
    Set up Paddles;
    Make Paddles Move;

---
+++ Pong/Assets/Projeto/Scripts/PaddleMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public GameObject paddle;
+    public float forceMultiplier = 5.0f;
+    public bool isPlayer1;
+    private Rigidbody2D rbPaddle;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rbPaddle = paddle.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Player1CheckMovement();
+        Player2CheckMovement();
+
+    }
+
+    #region Player1 methods
+    private void Player1CheckMovement()
+    {
+        if (Input.GetButton("Vertical1") && isPlayer1)
+        {
+            Player1Move();
+        }
+        if (Input.GetButton("Vertical1") && isPlayer1)
+        {
+            Player1Move();
+        }
+    }
+    private void Player1Move()
+    {
+        rbPaddle.AddForce(Input.GetAxis("Vertical1") * transform.up * forceMultiplier * Time.deltaTime, ForceMode2D.Impulse);
+    }
+    #endregion
+
+    #region Player2 Methods
+
+    private void Player2CheckMovement()
+    {
+        if (Input.GetButton("Vertical2") && !isPlayer1)
+        {
+            Player2Move();
+        }
+        if (Input.GetButton("Vertical2") && !isPlayer1)
+        {
+            Player2Move();
+        }
+    }
+
+    private void Player2Move()
+    {
+        rbPaddle.AddForce(Input.GetAxis("Vertical2") * transform.up * forceMultiplier * Time.deltaTime, ForceMode2D.Impulse);
+    }
+    #endregion
+
+}
